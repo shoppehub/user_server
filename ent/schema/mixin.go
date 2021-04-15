64e2280abcba27e770dfb8a00be87147ca0f028b
jiaoge <jiaoyingjun@youkeda.com>
2021-04-15 09:54:24 +0800

    add site db

---
+++ ent/schema/mixin.go
@@ -0,0 +1,30 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+)
+
+// -------------------------------------------------
+// Mixin definition
+
+// 创建和修改时间
+type GmtMixin struct {
+	// We embed the `mixin.Schema` to avoid
+	// implementing the rest of the methods.
+	mixin.Schema
+}
+
+func (GmtMixin) Fields() []ent.Field {
+	return []ent.Field{
+		field.Time("created_at").
+			Immutable().
+			Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
+}
